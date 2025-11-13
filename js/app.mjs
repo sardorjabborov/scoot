73231d2ab0c661a9022150da33103f6cf8b8f89f
SardorJabborov <sardorjabborov1994@gmail.com>
2025-11-13 22:18:35 +0300

    jsdjj

--- js/app.mjs
+++ js/app.mjs
@@ -35,7 +35,7 @@ function initSignupModal() {
     };
 
     try {
-      const res = await fetch("http://localhost:7777/api/signup", {
+      const res = await fetch("https://scoot-backend-1.onrender.com/api/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData)
